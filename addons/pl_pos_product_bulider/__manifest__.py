6792ec5a6c9a0089b2bc65238b307fc4ee3657b8
ahmedAtef-Mo <ahmed.atef.wer@gmail.com>
2022-11-16 12:24:07 +0200

    [Update]: init

---
+++ addons/pl_pos_product_bulider/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "pl_pos_product_bulider",
+    'summary': """
+    """,
+    'description': """
+    """,
+   'author': 'Plementus',
+    'website': 'https://www.plementus.com',
+
+    'contributors': [
+        '',
+    ],
+    'version': '0.1',
+    'depends': ['mrp', 'point_of_sale'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
---
+++ addons/pl_pos_product_bulider/models/model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#
+#    Copyright (C) 2019-TODAY .
+#    Author: Plementus <https://plementus.com>
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+##############################################################################
+
+from odoo import api, fields, models,_
+from odoo.exceptions import ValidationError,UserError
